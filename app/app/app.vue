d44c2ff6f4d99f8724c96f5894f2039aaab71a24
Denys Daneliuk <d.danelyuk@gmail.com>
2026-09-09 22:29:36 +0200

    init

---
+++ app/app/app.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Button from "./components/ui/button/Button.vue"
+import { ArrowUpIcon } from '@lucide/vue'
+</script>
+
+<template>
+  <div class="flex flex-wrap items-center gap-2 md:flex-row">
+    <Button variant="outline">
+      Button
+    </Button>
+    <Button variant="outline" size="icon" aria-label="Submit">
+      <ArrowUpIcon />
+    </Button>
+  </div>
+</template>
---
+++ app/tailwind.config.ts
@@ -0,0 +1,14 @@
+// tailwind.config.ts
+export default {
+  content: [
+    "./components/**/*.{js,vue,ts}",
+    "./pages/**/*.{js,vue,ts}",
+    "./layouts/**/*.{js,vue,ts}",
+    "./app.vue",
+    "./.nuxt/components.d.ts",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
